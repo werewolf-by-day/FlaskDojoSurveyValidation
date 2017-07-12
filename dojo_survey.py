fc9c277b3f5ea9c3fec96de18da2a4df31dcbdb2
Jason <jason.w.switzer@gmail.com>
2017-07-11 19:04:00 -0700

    completed dojo survey with validation

--- dojo_survey.py
+++ dojo_survey.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, flash
 app = Flask(__name__)
 app.secret_key = 'KeepItSecretKeepItSafe'
 
@@ -8,6 +8,21 @@ def index():
 
 @app.route('/process', methods=['Post'])
 def process():
+    if len(request.form['textarea']) > 120:
+        flash("Comments cannot be more than 120 characters!")
+        return redirect('/')
+    else:
+        pass
+    if len(request.form['textarea']) < 1:
+        flash("Comments cannot be empty!")
+        return redirect('/')
+    else:
+        pass
+    if len(request.form['name']) < 1:
+        flash("Name cannot be empty!")
+        return redirect('/')
+    else:
+        pass
     comment = request.form['textarea']
     return render_template('results.html', comment = comment)
 
